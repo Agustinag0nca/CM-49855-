5dd6a5206c7b1cdf82b5ac42593461a73cc0cb18
Agustinag0nca <146680809+Agustinag0nca@users.noreply.github.com>
2024-02-25 10:45:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Manejar el envío del formulario de contacto
-    let formContacto = document.getElementById("formularioContacto");
-    if (formContacto) {
-        formContacto.addEventListener("submit", function(event) {
+    document.addEventListener("DOMContentLoaded", function() {
+
+    // Manejar el envío del formulario de inicio de sesión
+    let formLogin = document.getElementById("miForm");
+    if (formLogin) {
+        formLogin.addEventListener("submit", function(event) {
             event.preventDefault(); // Evitar que se envíe el formulario automáticamente
 
-            // Obtener los valores del formulario
-            let nombre = document.getElementById("nombre").value;
-            let correo = document.getElementById("correo").value;
-            let mensaje = document.getElementById("mensaje").value;
-
-            // Mostrar mensaje de éxito al usuario
+            // Mostrar mensaje de inicio de sesión
             Swal.fire({
-                title: 'Mensaje enviado',
-                text: '¡Gracias por ponerte en contacto con nosotros!',
+                title: 'Inicio de sesión',
+                text: '¡Has iniciado sesión correctamente!',
                 icon: 'success',
-                confirmButtonText: 'Ok'
+                confirmButtonText: 'Ok',
             });
-
-            // Limpiar los campos del formulario
-            formContacto.reset();
         });
     }
+
+        // Manejar el envío del formulario de contacto
+        let formContacto = document.getElementById("formularioContacto");
+        if (formContacto) {
+            formContacto.addEventListener("submit", function(event) {
+                event.preventDefault(); // Evitar que se envíe el formulario automáticamente
+    
+                // Obtener los valores del formulario
+                let nombre = document.getElementById("nombre").value;
+                let correo = document.getElementById("correo").value;
+                let mensaje = document.getElementById("mensaje").value;
+    
+                // Aquí puedes agregar la lógica para enviar los datos del formulario (por ejemplo, a través de una solicitud AJAX)
+    
+                // Mostrar mensaje de éxito al usuario después de 2 segundos
+                setTimeout(function() {
+                    Swal.fire({
+                        title: 'Mensaje enviado',
+                        text: '¡Gracias por ponerte en contacto con nosotros!',
+                        icon: 'success',
+                        confirmButtonText: 'Ok'
+                    });
+                }, 2000);
+    
+                // Limpiar los campos del formulario después de 2 segundos
+                setTimeout(function() {
+                    formContacto.reset();
+                }, 2000);
+            });
+        }
 
     // Función para mostrar los precios de los servicios en una ventana emergente
     function mostrarPrecios() {
@@ -48,21 +71,56 @@ document.addEventListener("DOMContentLoaded", function() {
         return preciosText;
     }
 
-    // Manejar el envío del formulario de inicio de sesión
-    let formLogin = document.getElementById("miForm");
-    if (formLogin) {
-        formLogin.addEventListener("submit", function(event) {
-            event.preventDefault(); // Evitar que se envíe el formulario automáticamente
+    // Función para obtener el precio de un servicio por su nombre
+    function obtenerPrecioPorNombre(nombre) {
+        switch (nombre) {
+            case "Maquetación Web":
+                return 90000;
+            case "Estética Web":
+                return 40000;
+            case "Dominio Original":
+                return 80000;
+            case "Solicitar Arreglos":
+                return 50000;
+            case "Crear una Marca de Cero":
+                return 100000;
+            ; // Precio por defecto si el servicio no está definido
+        }
+    }
 
-            // Mostrar mensaje de inicio de sesión
-            Swal.fire({
-                title: 'Inicio de sesión',
-                text: '¡Has iniciado sesión correctamente!',
-                icon: 'success',
-                confirmButtonText: 'Ok',
+    document.addEventListener("DOMContentLoaded", function() {
+        const carrito = []; // Array para almacenar los productos agregados al carrito
+    
+        // Función para actualizar el contador de ítems en el icono del carrito
+        function actualizarContadorCarrito() {
+            const contadorCarrito = document.getElementById('contadorCarrito');
+            contadorCarrito.textContent = carrito.length;
+        }
+    
+        // Función para agregar un producto al carrito
+        function agregarAlCarrito(nombre, precio) {
+            carrito.push({ nombre, precio });
+            actualizarContadorCarrito();
+        }
+    
+        // Agregar evento a los botones de agregar al carrito
+        const botonesAgregarCarrito = document.querySelectorAll('.agregar-carrito');
+        botonesAgregarCarrito.forEach(boton => {
+            boton.addEventListener('click', function() {
+                const nombre = boton.dataset.nombre;
+                const precio = parseFloat(boton.dataset.precio);
+                agregarAlCarrito(nombre, precio);
+    
+                // Mostrar mensaje de éxito al usuario
+                Swal.fire({
+                    title: 'Agregado al carrito',
+                    text: `${nombre} se ha añadido al carrito.`,
+                    icon: 'success',
+                    confirmButtonText: 'Ok'
+                });
             });
         });
-    }
+    });
 
     // Agregar evento al botón de consulta
     let consultaButton = document.getElementById("consultaButton");
@@ -120,70 +178,8 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    // Función para mostrar los productos seleccionados
     function mostrarProductos(design, businessType) {
         console.log('Diseño seleccionado:', design);
         console.log('Tipo de negocio seleccionado:', businessType);
     }
-
-    // Función para mostrar notificaciones Toastify con estilos
-    let totalItemsCarrito = 0;
-
-    // Función para actualizar el contador de ítems en el icono del carrito
-    function actualizarContadorCarrito() {
-        const contadorCarrito = document.getElementById('contadorCarrito');
-        contadorCarrito.textContent = totalItemsCarrito;
-    }
-
-    // Función para mostrar notificaciones Toastify con estilos
-    function mostrarNotificacion(texto) {
-        var notificacion = document.createElement('div');
-        notificacion.textContent = texto;
-        notificacion.style.backgroundColor = '#007bff';
-        notificacion.style.color = '#fff';
-        notificacion.style.borderRadius = '5px';
-        notificacion.style.fontFamily = 'Arial, sans-serif';
-        notificacion.style.fontSize = '16px';
-        notificacion.style.padding = '10px 20px';
-        notificacion.style.marginBottom = '10px';
-        notificacion.style.position = 'fixed';
-        notificacion.style.top = '20px';
-        notificacion.style.right = '20px';
-        notificacion.style.zIndex = '1000';
-        notificacion.style.animation = 'push-up 0.5s ease-in-out'; // Animación push-up
-        document.body.appendChild(notificacion);
-        setTimeout(function() {
-            notificacion.remove(); // Eliminar la notificación después de 3 segundos
-        }, 3000);
-    }
-
-    // Función para agregar un ítem al carrito
-    function agregarItemAlCarrito(item) {
-        const listaCarrito = document.getElementById('listaCarrito');
-        const elemento = document.createElement('li');
-        elemento.textContent = `${item.nombre} - $${item.precio.toFixed(2)}`;
-        listaCarrito.appendChild(elemento);
-
-        // Incrementar el contador de ítems en el carrito
-        totalItemsCarrito++;
-        // Actualizar visualmente el contador de ítems en el icono del carrito
-        actualizarContadorCarrito();
-
-        // Mostrar notificación Toastify
-        mostrarNotificacion(`Se ha añadido ${item.nombre} al carrito.`);
-    }
-
-    // Agregar evento a los botones de agregar al carrito
-    const productos = document.querySelectorAll('.agregar-carrito');
-    productos.forEach(producto => {
-        producto.addEventListener('click', function(event) {
-            const boton = event.target;
-            const nombre = boton.dataset.nombre;
-            const precio = parseFloat(boton.dataset.precio);
-
-            // Añadir producto al carrito
-            const item = { nombre, precio };
-            agregarItemAlCarrito(item);
-        });
-    });
 });
