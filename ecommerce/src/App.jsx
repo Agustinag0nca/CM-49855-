3f6cc3ee37e6f3b32023b2a8e8002f58c8c68947
Agustinag0nca <agustinagonca@gmail.com>
2024-03-09 17:44:34 -0300

    changes

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <NavBar />
+      </div>
+      <h1>Carrito de Compras</h1>
+    </>
+  );
+}
+
+export default App;
